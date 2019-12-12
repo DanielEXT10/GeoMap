6a0b2a0795866297ac65556233807d50954002a4
Daniel Alfonso Garcia Perez <DPerez48@slb.com>
2019-12-12 17:49:39 -0600

    Post-Fetch

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () =>{
+    console.log('Server on Port 3000');
+    
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+app.post('/api', (req,res)=>{
+    console.log('I got a request');
+    console.log(req.body);
+    const data = req.body
+    res.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    });
+});
