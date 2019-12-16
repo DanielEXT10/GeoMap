ff28dda053c8bf77d3ebeb9a6c17bca59619f7d6
Daniel Alfonso Garcia Perez <DPerez48@slb.com>
2019-12-16 17:51:29 -0600

    add database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 
 app.listen(3000, () =>{
     console.log('Server on Port 3000');
@@ -8,12 +9,20 @@ app.listen(3000, () =>{
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb'}));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
 app.post('/api', (req,res)=>{
     console.log('I got a request');
     console.log(req.body);
     const data = req.body
+    database.insert(data);
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
     res.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat,
         longitude: data.lon
     });
